33fb284c55b892640e517bc41fbaaea1972ec459
Vasilii Chugunov <naukomania@gmail.com>
2017-05-20 14:32:17 +0300

    Added snake

--- js/warheroes.js
+++ js/warheroes.js
@@ -1,4 +1,44 @@
-
+function Snake(){
+    //спрайты
+    var me = this;
+    var snakeSprites = new createjs.SpriteSheet({
+        "animations": {
+            "walkRight":{
+                "frames":[3,4,5],
+                "speed": 0.1
+            },
+            "walkLeft":{
+                "frames": [9,10,11],
+                "speed": 0.1
+            },
+            "stand":{
+                "frames": [6,7,8,7],
+                "speed": 0.1
+            },
+            "wounding":{
+                "frames":[6,7,8],
+                "speed": 0.1
+            }
+        },
+        "images":["images/king_cobra.png"],
+        "frames":{
+            "height": 63,
+            "width": 63,
+            "regX": 0,
+            "regY": 0
+        }
+    })
+    //добавление змеи на сцену
+    this.snakeAnimation = new createjs.Sprite(snakeSprites);
+    game.stage.addChild(this.snakeAnimation);   
+    this.snakeAnimation.gotoAndPlay("walkLeft");
+    //this.snakeAnimation.x
+    createjs.Tween.get(this.snakeAnimation)
+    .to({x:150, y:150});
+    // addEventListener("click", function(event){
+    //     me.snakeAnimation.gotoAndPlay("walkLeft")
+    // })
+}
 
 function Knight(){
     var me = this;
@@ -30,7 +70,7 @@ function Knight(){
         me.direction = (x > me.imgObj.x)+0;
         me.imgObj.gotoAndPlay(me.dirSettings[me.direction]["walk"])
         createjs.Tween.get(me.imgObj)
-            .to({ x: x, y: y}, parseInt(Math.abs(x - me.imgObj.x) / 0.3), createjs.Ease.getPowInOut(2))
+            .to({ x: x - 42.5 , y: y - 42.5}, parseInt(Math.abs(x - me.imgObj.x) / 0.3), createjs.Ease.getPowInOut(2))
             .call(function(){
                 me.imgObj.gotoAndStop(me.dirSettings[me.direction]["walk"]);
             });
@@ -52,6 +92,7 @@ function Game(stageId){
 
         this.start = function(){
         me.knight = new Knight();
+        me.snake = new Snake();
         me.stage.update();
         // createjs.Tween.get(circle, { loop: true })
         // .to({ x: 1000 }, 1000, createjs.Ease.getPowInOut(4))
